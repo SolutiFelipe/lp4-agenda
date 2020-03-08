b1185e5608d318a95951fc2886236a79fff89d74
haganicolau <haganicolau@gmail.com>
2020-03-08 17:15:45 -0300

    criando lista de contatos

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,87 @@
 import 'package:flutter/material.dart';
 
-int _cliques = 0;
-void main() => runApp(
-  MaterialApp(
-    home: Scaffold(
+void main() => runApp(AgendaApp());
+
+class AgendaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: ListaPessoas(),
+      ),
+    );
+  }
+
+}
+
+class ListaPessoas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Título Primeiro App"),
+        title: Text("Agenda"),
       ),
-      body: Center(
-        child: Text(
-          "Testando Body!",
-          style: TextStyle(
-            fontSize: 24,
+      body: ListView(
+        children: <Widget>[
+          ItemPessoa(
+            Pessoa(
+              "Bruce Wayne",
+              "Empresário",
+            ),
           ),
-        ),
+          ItemPessoa(
+            Pessoa(
+              "Carol Danvers",
+              "Piloto",
+            ),
+          ),
+          ItemPessoa(
+            Pessoa(
+              "Peter Park",
+              "Fotógrafo",
+            ),
+          ),
+          ItemPessoa(
+            Pessoa(
+              "Tony Start",
+              "Empresário",
+            ),
+          ),
+        ],
       ),
-      backgroundColor: Colors.white70,
       floatingActionButton: FloatingActionButton(
-        onPressed: ()  {
-          debugPrint('clicou no botão');
-          _cliques++;
-          debugPrint(_cliques.toString());
+        onPressed: () {
+          debugPrint("clicou no botão");
         },
         child: const Icon(Icons.add),
       ),
-    ),
-  ),
-);
+    );
+  }
+}
+
+class ItemPessoa extends StatelessWidget {
+  Pessoa _pessoa;
+
+  ItemPessoa(this._pessoa);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        title: Text(this._pessoa._nome),
+        subtitle: Text(this._pessoa._profissao),
+        leading: Icon(Icons.person),
+      ),
+    );
+  }
+}
+
+class Pessoa {
+  String _nome;
+  String _profissao;
+
+  Pessoa(this._nome, this._profissao);
+}
